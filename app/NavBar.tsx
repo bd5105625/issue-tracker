459f37a587019ba88b1607894f1c9346747c229a
Brad Liu <bd5105625@gmail.com>
2023-12-20 20:31:58 +0800

    feat: set up prisma

---
+++ app/NavBar.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Link from 'next/link'
+import React from 'react'
+import { usePathname } from 'next/navigation'
+import { AiFillBug } from 'react-icons/ai'
+import classnames from 'classnames'
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath)
+  const lists = [
+    { name: 'Dashboard', href: '/' },
+    { name: 'Issues', href: '/issues' }
+  ]
+  return (
+    <nav className='flex space-x-5 border-b mb-3 px-5 h-14 items-center'>
+      <Link href="/"><AiFillBug/></Link>
+      <ul className='flex space-x-5'>
+        {lists.map(list => 
+          <Link 
+            key={list.href}
+            className={classnames({
+              "text-zinc-900": list.href === currentPath,
+              "text-zinc-400": list.href !== currentPath,
+              "hover:text-zinc-600 translate-colors": true
+            })}
+            href={list.href}>{list.name}
+          </Link>)}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
