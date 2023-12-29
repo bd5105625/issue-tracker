00d51d47957c671c8661f56abaae4dadce71bec7
Brad Liu <bd5105625@gmail.com>
2023-12-29 21:58:01 +0800

    feat: build issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,27 @@
+import { Status } from '@prisma/client'
+import { Badge } from '@radix-ui/themes'
+import React from 'react'
+
+// interface Props {
+//   status: Status
+// }
+
+
+// Record is an utility in TS let us to declare specific type in key-value pair
+const statusMap: Record<
+  Status, 
+  { label:string, color: 'red' | 'violet' | 'green'}
+> = {
+  OPEN: { label: 'Open', color: 'red'},
+  IN_PROGRESS: { label: 'In Progress', color: 'violet'},
+  CLOSED: { label: 'CLOSED', color: 'green'}
+}
+
+const IssueStatusBadge = ({ status } : { status: Status }) => {
+
+  return (
+    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+  )
+}
+
+export default IssueStatusBadge
