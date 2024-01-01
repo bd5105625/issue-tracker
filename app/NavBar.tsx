472d94b891f07e9899494507901514565bd574d9
Brad Liu <bd5105625@gmail.com>
2024-01-01 23:30:34 +0800

    feat: styling navbar with flex tag

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
 import classnames from 'classnames'
 import { useSession } from 'next-auth/react'
-import { Box } from '@radix-ui/themes'
+import { Box, Container, Flex } from '@radix-ui/themes'
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -15,32 +15,39 @@ const NavBar = () => {
   ]
   const {status, data: session} = useSession();
   return (
-    <nav className='flex space-x-5 border-b mb-3 px-5 h-14 items-center'>
-      <Link href="/"><AiFillBug/></Link>
-      <ul className='flex space-x-5'>
-        {lists.map(list => (
-          <li key={list.href}>
-            <Link 
-              className={classnames({
-                "text-zinc-900": list.href === currentPath,
-                "text-zinc-400": list.href !== currentPath,
-                "hover:text-zinc-600 translate-colors": true
-              })}
-              href={list.href}>{list.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        { status === 'authenticated' && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        { status === 'unauthenticated' && (
-          <Link href="/api/auth/signin">Login</Link>
-        )
-
-        }
-      </Box>
+    <nav className='border-b mb-3 px-5 py-3 items-center'>
+      <Container>
+        <Flex justify='between'>
+          <Flex align='center' gap='5'>
+            <Link href="/"><AiFillBug/></Link>
+            <ul className='flex space-x-5'>
+              {lists.map(list => (
+                <li key={list.href}>
+                  <Link 
+                    className={classnames({
+                      "text-zinc-900": list.href === currentPath,
+                      "text-zinc-400": list.href !== currentPath,
+                      "hover:text-zinc-600 translate-colors": true
+                    })}
+                    href={list.href}>{list.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            { status === 'authenticated' && (
+              <Flex gap="2">
+                <h1>{session.user?.name}</h1>
+                <Link href="/api/auth/signout">Log out</Link>
+              </Flex>
+            )}
+            { status === 'unauthenticated' && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   )
 }
