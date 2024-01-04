eabd8a26846146a46cdb486bb85727ae059bdc40
Brad Liu <bd5105625@gmail.com>
2024-01-04 21:08:03 +0800

    feat: Enhance the API to assign issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,65 +1,79 @@
-import { issueSchema } from "@/app/validationSchema";
-import { NextRequest, NextResponse } from "next/server";
-import prisma from '@/prisma/client'
-import delay from "delay";
-import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/AuthOptions";
+import { patchIssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
-  request: NextRequest, 
-  { params } : { params: { id:string }}) {
-
-  const session = await getServerSession(authOptions)
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
   if (!session) {
-    return NextResponse.json({}, { status: 401 })
+    return NextResponse.json({}, { status: 401 });
   }
-  const body = await request.json()
-  const validation = issueSchema.safeParse(body)
-  if (!validation.success) 
-    return NextResponse.json(validation.error.format(), { status: 400 })
+
+  const body = await request.json();
+  const validation = patchIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { title, description, assignedToUserId } = body;
+
+  // if there is a user id -> check whether the id is valid
+  if (assignedToUserId) {
+    const user = prisma.user.findUnique({
+      where: {
+        id: assignedToUserId,
+      },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid User." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
-      id: parseInt(params.id)
-    }
-  })
+      id: parseInt(params.id),
+    },
+  });
   if (!issue)
-    return NextResponse.json({ error: 'Invalid Issue' }, { status: 404 })
-  
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+
+  // since three element are optional in this schema, 
+  // just include all of them even it is undefined(null), it still works
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description
-    }
-  })
-  return NextResponse.json(updatedIssue)
-
+      title,
+      description,
+      assignedToUserId
+    },
+  });
+  return NextResponse.json(updatedIssue);
 }
 
 export async function DELETE(
-  request: NextRequest, 
-  { params } : { params: { id:string }}) {
-
-  const session = await getServerSession(authOptions)
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
   if (!session) {
-    return NextResponse.json({}, { status: 401 })
+    return NextResponse.json({}, { status: 401 });
   }
   const issue = await prisma.issue.findUnique({
     where: {
-      id: parseInt(params.id)
-    }
-  })
-  
-  if(!issue) {
-    return NextResponse.json({ error: "Invalid Issue"}, { status: 404})
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
   }
 
   await prisma.issue.delete({
     where: {
-      id: issue.id
-    }
-  })
-  return NextResponse.json({})
-
+      id: issue.id,
+    },
+  });
+  return NextResponse.json({});
 }
