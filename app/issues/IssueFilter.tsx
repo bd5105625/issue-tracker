315189087afc9ab39e4915176ef53e52cf146ff4
Brad Liu <bd5105625@gmail.com>
2024-01-05 16:39:58 +0800

    fix: query for status and orderBy

--- app/issues/IssueFilter.tsx
+++ app/issues/IssueFilter.tsx
@@ -1,11 +1,12 @@
 'use client'
 import { Status } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const IssueFilter = () => {
   const route = useRouter();
+  const searchParams = useSearchParams();
 
   const statuses: { label: string, value?: Status}[] = [
     { label: 'All' },
@@ -14,15 +15,20 @@ const IssueFilter = () => {
     { label: 'Closed', value: 'CLOSED'},
   ]
   return (
-    <Select.Root onValueChange={(status) => {
-      const query = status !== 'null' ? `?status=${status}` : ''
+    // defaultValue parameter are linked to value in the select.content, that's why '' is equally to all(from statuses object)
+    <Select.Root defaultValue={searchParams.get('status') || 'ALL'} onValueChange={(status) => {
+      const params = new URLSearchParams();
+      if (status) params.append('status', status)
+      if (searchParams.get('orderBy')) params.append('orderBy', searchParams.get('orderBy')!)
+
+      const query = params.size ? '?' + params.toString() : ''
       route.push('issues' + query)
 
     }}>
-      <Select.Trigger placeholder='Filter by status...' />
+      <Select.Trigger placeholder="Filter by status..."/>
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.label} value={status.value || "null"}>
+          <Select.Item key={status.label} value={status.value || 'ALL'}>
             {status.label}
           </Select.Item>
         ))}
