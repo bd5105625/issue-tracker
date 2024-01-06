b15302c5eb414e7bb25da8bf68e9be26f328f399
Brad Liu <bd5105625@gmail.com>
2024-01-07 00:37:33 +0800

    feat: Build BarChart

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import prisma from '@/prisma/client'
 import IssueSummary from './IssueSummary'
+import IssueChart from './IssueChart'
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -14,6 +15,6 @@ export default async function Home() {
   
   return (
     // <LatestIssue />
-    <IssueSummary open={open} in_progress={inProgress} closed={closed} />
+    <IssueChart open={open} inProgress={inProgress} closed={closed} />
   )
 }
