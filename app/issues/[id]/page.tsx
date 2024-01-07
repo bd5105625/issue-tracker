20f64a06c2a3777d53ac8bd4b27630b07db9f523
Brad Liu <bd5105625@gmail.com>
2024-01-07 11:45:30 +0800

    perf: Optimized performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,6 +7,7 @@ import DeleteIssueButton from './DeleteIssueButton'
 import { getServerSession } from 'next-auth'
 import authOptions from '@/app/auth/AuthOptions'
 import AssigneeSelect from './AssigneeSelect'
+import { cache } from 'react'
 
 interface Props {
   params: {
@@ -14,15 +15,14 @@ interface Props {
   }
 }
 
-const IssueDetailPage = async ({ params } : Props) => {
+// use cache to store issue data instead of always read data when the page loaded -> optimized performance
+const fetchIssue = cache((issueId: number) => prisma.issue.findUnique({ where: { id: issueId } }))
 
-  const session = await getServerSession(authOptions);
+const IssueDetailPage = async ({ params }: Props) => {
+
   // if (typeof params.id !== 'number') notFound(); // handle wrong id error from url
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(params.id)
-    }
-  })
+  const session = await getServerSession(authOptions);
+  const issue = await fetchIssue(parseInt(params.id))
 
   // if issue not found -> redirect to not found page
   if (!issue)
@@ -30,14 +30,14 @@ const IssueDetailPage = async ({ params } : Props) => {
 
   return (
     // md(in radix UI) is equivalent to lg in tailwind (md=lg, sm=md)
-    <Grid columns={{ initial: '1', sm: '5'}} gap={'5'}>
+    <Grid columns={{ initial: '1', sm: '5' }} gap={'5'}>
       <Box className='md:col-span-4'>
         <IssueDetail issue={issue} />
       </Box>
       {session && <Box>
         <Flex direction="column" gap="4">
-          <AssigneeSelect issue={issue}/>
-          <EditButton issueId={issue.id}/>
+          <AssigneeSelect issue={issue} />
+          <EditButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </Flex>
       </Box>}
@@ -48,10 +48,10 @@ const IssueDetailPage = async ({ params } : Props) => {
 export const dynamic = 'force-dynamic'
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id)}})
+  const issue = await fetchIssue(parseInt(params.id))
 
   return {
-    title: issue?.title, 
+    title: issue?.title,
     description: issue?.id
   }
 }
